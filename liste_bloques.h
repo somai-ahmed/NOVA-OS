993f2d86202106043206c39b9b05990f6e097344
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-06-22 17:08:21 +0100

    blocked process list

---
+++ liste_bloques.h
@@ -0,0 +1,22 @@
+#ifndef LISTE_BLOQUES_H
+#define LISTE_BLOQUES_H
+#include "processus.h"
+
+typedef struct structnoeud { //liste doublement chainee
+    Processus        info;
+    struct structnoeud *suivant;
+    struct structnoeud *precedent;
+}structnoeud ,*NOEUD;
+
+typedef struct lastruct{
+    NOEUD tete;
+    NOEUD queue;
+    int       taille;
+}*ListeBloques;
+
+void      creerListeBloques(ListeBloques l);
+void      insererBloque(ListeBloques l, Processus p);
+Processus retirerBloque(ListeBloques l, pid_t pid);
+void      afficherBloques(ListeBloques l);
+
+#endif
