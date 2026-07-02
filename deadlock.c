fa574cc88e3c7e7258a29af6a7d97bf6a61447fc
Ahmed Somai <ahmeedsomaii06@gmail.com>
2026-07-02 02:27:41 +0100

    Implement resource need calculation in calculerBesoin
    Updated the calculerBesoin function to calculate resource needs based on max and allocation matrices.

--- deadlock.c
+++ deadlock.c
@@ -1,5 +1,12 @@
 #include "deadlock.h"
 
-void calculerBesoin(EtatSysteme *s){}
+void calculerBesoin(EtatSysteme s){
+    for(int i = 0 ; i<NB_PROC ; i++){
+        for (int j=0 j<NB_RES ; j++){
+            s->besoin[i][j] = s->max[i][j] - s->allocation[i][j];
+        }
+    }
+}
+
 int  estEtatSur(EtatSysteme *s, int seq[NB_PROC]){}
 
